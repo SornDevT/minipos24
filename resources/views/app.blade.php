061ccf9b111961075df3501f7a323484ba3ada45
SorndevTeach <sorndev.teach@gmail.com>
2025-06-23 22:27:59 +0700

    install git, install template to project VDO 02

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,15 +6,45 @@
     <title>Document</title>
 
     @vite('resources/css/app.css')
+
+    <!-- build:css assets/vendor/css/theme.css  -->
+    
+    <link rel="stylesheet" href="../assets/vendor/css/core.css">
+    <link rel="stylesheet" href="../assets/css/demo.css">
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
    
 </head>
 <body>
     
-    <h1>Laravel 12</h1>
-    <hr>
     <div id="app" ></div>
 
 
      @vite('resources/js/app.js')
+
+
+         <!-- Core JS -->
+    
+    
+    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/vendor/js/bootstrap.js"></script>
+
+    
+
+    
+      <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+      
+      <script src="../assets/vendor/js/menu.js"></script>
+    
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
+
+    <!-- Main JS -->
+    
+      <script src="../assets/js/main.js"></script>
+
 </body>
 </html>
